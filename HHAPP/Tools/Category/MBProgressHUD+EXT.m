8a068628f465198447ac06aa2a8e2f4ea428eff5
Now <now@liuxiaohuideMacBook-Pro.local>
2019-09-23 16:17:42 +0800

    format

--- HHAPP/Tools/Category/MBProgressHUD+EXT.m
+++ HHAPP/Tools/Category/MBProgressHUD+EXT.m
@@ -10,33 +10,39 @@
 
 //#import <AppKit/AppKit.h>
 
-
 @implementation MBProgressHUD (EXT)
 #pragma mark 显示错误信息
 + (void)showError:(NSString *)error {
     [self showError:error ToView:nil];
 }
-+ (void)showError:(NSString *)error ToView:(UIView *)view{
+
++ (void)showError:(NSString *)error ToView:(UIView *)view {
     [self showCustomIcon:@"MBHUD_Error" Title:error ToView:view];
 }
+
 + (void)showSuccess:(NSString *)success
 {
     [self showSuccess:success ToView:nil];
 }
+
 + (void)showSuccess:(NSString *)success ToView:(UIView *)view
 {
     [self showCustomIcon:@"MBHUD_Success" Title:success ToView:view];
 }
+
 + (void)showInfo:(NSString *)Info {
     [self showInfo:Info ToView:nil];
 }
+
 + (void)showInfo:(NSString *)Info ToView:(UIView *)view
 {
     [self showCustomIcon:@"MBHUD_Info" Title:Info ToView:view];
 }
+
 + (void)showWarn:(NSString *)Warn {
     [self showWarn:Warn ToView:nil];
 }
+
 + (void)showWarn:(NSString *)Warn ToView:(UIView *)view
 {
     [self showCustomIcon:@"MBHUD_Warn" Title:Warn ToView:view];
@@ -44,44 +50,46 @@
 
 #pragma mark 显示一些信息
 + (MBProgressHUD *)showMessage:(NSString *)message ToView:(UIView *)view {
-    if (view == nil) view = (UIView*)[UIApplication sharedApplication].delegate.window;
+    if (view == nil) view = (UIView *)[UIApplication sharedApplication].delegate.window;
     // 快速显示一个提示信息
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
     hud.mode = MBProgressHUDModeIndeterminate;
     hud.label.text = message;
     hud.label.numberOfLines = 0;
-    hud.label.font=[UIFont systemFontOfSize:14];;
+    hud.label.font = [UIFont systemFontOfSize:14];
     hud.bezelView.backgroundColor = [UIColor blackColor];
     hud.contentColor = [UIColor whiteColor];
     // 隐藏时候从父控件中移除
     hud.removeFromSuperViewOnHide = YES;
     //代表需要蒙版效果
-    
+
 //    hud.dimBackground = YES;
     return hud;
 }
+
 + (void)showHUD {
     [self showLoadToView:nil];
 }
+
 + (void)showHUDInfo:(NSString *)info {
     [self showMessage:info ToView:nil];
 }
+
 //加载视图
-+(void)showLoadToView:(UIView *)view{
++ (void)showLoadToView:(UIView *)view {
     [self showMessage:@"加载中..." ToView:view];
 }
-
 
 /**
  *  进度条View
  */
-+ (MBProgressHUD *)showProgressToView:(UIView *)view Text:(NSString *)text{
++ (MBProgressHUD *)showProgressToView:(UIView *)view Text:(NSString *)text {
     [MBProgressHUD hideHUD];
-    if (view == nil) view = (UIView*)[UIApplication sharedApplication].delegate.window;
+    if (view == nil) view = (UIView *)[UIApplication sharedApplication].delegate.window;
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    hud.label.text=text;
+    hud.label.text = text;
     hud.mode = MBProgressHUDModeDeterminateHorizontalBar;
-    hud.label.font=[UIFont systemFontOfSize:14];
+    hud.label.font = [UIFont systemFontOfSize:14];
     hud.bezelView.backgroundColor = [UIColor blackColor];
     hud.contentColor = [UIColor whiteColor];
     // 代表需要蒙版效果
@@ -89,36 +97,32 @@
     return hud;
 }
 
-
 //快速显示一条提示信息
-+ (void)showAutoMessage:(NSString *)message{
-    
++ (void)showAutoMessage:(NSString *)message {
     [self showAutoMessage:message ToView:nil];
 }
 
-
 //自动消失提示，无图
-+ (void)showAutoMessage:(NSString *)message ToView:(UIView *)view{
++ (void)showAutoMessage:(NSString *)message ToView:(UIView *)view {
     [self showMessage:message ToView:view RemainTime:1 Model:MBProgressHUDModeText];
 }
 
 //自定义停留时间，有图
-+(void)showIconMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time{
++ (void)showIconMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time {
     [self showMessage:message ToView:view RemainTime:time Model:MBProgressHUDModeIndeterminate];
 }
 
 //自定义停留时间，无图
-+(void)showMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time{
++ (void)showMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time {
     [self showMessage:message ToView:view RemainTime:time Model:MBProgressHUDModeText];
 }
 
-+(void)showMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time Model:(MBProgressHUDMode)model{
-    
-    if (view == nil) view = (UIView*)[UIApplication sharedApplication].delegate.window;
++ (void)showMessage:(NSString *)message ToView:(UIView *)view RemainTime:(CGFloat)time Model:(MBProgressHUDMode)model {
+    if (view == nil) view = (UIView *)[UIApplication sharedApplication].delegate.window;
     // 快速显示一个提示信息
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    hud.label.text=message;
-    hud.label.font=[UIFont systemFontOfSize:15];;
+    hud.label.text = message;
+    hud.label.font = [UIFont systemFontOfSize:15];
     hud.label.numberOfLines = 0;
     //模式
     hud.mode = model;
@@ -133,16 +137,15 @@
     // X秒之后再消失
 //    [hud hide:YES afterDelay:time];
     [hud hideAnimated:YES afterDelay:time];
-    
 }
 
 + (void)showCustomIcon:(NSString *)iconName Title:(NSString *)title ToView:(UIView *)view
 {
-    if (view == nil) view = (UIView*)[UIApplication sharedApplication].delegate.window;
+    if (view == nil) view = (UIView *)[UIApplication sharedApplication].delegate.window;
     // 快速显示一个提示信息
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    hud.label.text=title;
-    hud.label.font= [UIFont systemFontOfSize:14];
+    hud.label.text = title;
+    hud.label.font = [UIFont systemFontOfSize:14];
     hud.label.numberOfLines = 0;
     // 设置图片
     hud.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:iconName]];
@@ -150,13 +153,10 @@
     hud.contentColor = [UIColor whiteColor];
     // 再设置模式
     hud.mode = MBProgressHUDModeCustomView;
-    
     // 隐藏时候从父控件中移除
     hud.removeFromSuperViewOnHide = YES;
-    
     // 代表需要蒙版效果
 //    hud.dimBackground = YES;
-    
     // 3秒之后再消失
 //    [hud hide:YES afterDelay:1];
     [hud hideAnimated:YES afterDelay:1];
@@ -164,7 +164,7 @@
 
 + (void)hideHUDForView:(UIView *)view
 {
-    if (view == nil) view = (UIView*)[UIApplication sharedApplication].delegate.window;
+    if (view == nil) view = (UIView *)[UIApplication sharedApplication].delegate.window;
     [self hideHUDForView:view animated:YES];
 }
 
